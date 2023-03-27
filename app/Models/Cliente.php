ce344914116f044d5bff69842f14f53427c77ebe
Armando890 <armandiuskigutierrez@gmail.com>
2023-03-27 09:29:23 -0600

    salvando el parcial con un crud:(

---
+++ app/Models/Cliente.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Cliente
+ *
+ * @property $id
+ * @property $name
+ * @property $email
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Usuario[] $usuarios
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Cliente extends Model
+{
+    
+    static $rules = [
+		'name' => 'required',
+		'email' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name','email'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function usuarios()
+    {
+        return $this->hasMany('App\Models\Usuario', 'cliente_id', 'id');
+    }
+    
+
+}
---
+++ resources/views/usuario/show.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('template_title')
+    {{ $usuario->name ?? "{{ __('Show') Usuario" }}
+@endsection
+
+@section('content')
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="float-left">
+                            <span class="card-title">{{ __('Show') }} Usuario</span>
+                        </div>
+                        <div class="float-right">
+                            <a class="btn btn-primary" href="{{ route('usuarios.index') }}"> {{ __('Back') }}</a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        
+                        <div class="form-group">
+                            <strong>Precio:</strong>
+                            {{ $usuario->precio }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Cliente Id:</strong>
+                            {{ $usuario->cliente_id }}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
